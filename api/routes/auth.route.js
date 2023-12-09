57cefcddcc0d7241dc7f03c48e7f8dec8014db8c
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-09 23:24:11 +0100

    Add google auth api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signIn, signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp, googleAuth } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.post("/g-auth", googleAuth);
 
 export default router;
