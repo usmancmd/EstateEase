a47af92c365ec0883835599f0e206d8d71bb8eb6
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-09 23:16:36 +0100

    Add customErrorHandler for handling errors

---
+++ api/middleware/middleware.js
@@ -0,0 +1,9 @@
+export const customErrorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+};
