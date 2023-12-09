628fc5762fc23fbe4380e402193eefa9f346d724
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-09 12:33:29 +0100

    Hash user password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
 export const signUp = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json("user created successfully!");
