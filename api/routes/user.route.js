3da575c8e7019ce78d08e622c450c8aa26efd677
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-10 00:24:18 +0100

    create deleteUser api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { deleteUser, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
