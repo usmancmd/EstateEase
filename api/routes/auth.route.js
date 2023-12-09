7a527998826ec0ceb1bdbb08db1968c9ffef8bf6
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-10 00:23:10 +0100

    create signout api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { signIn, signUp, googleAuth } from "../controllers/auth.controller.js";
+import {
+  signIn,
+  signUp,
+  googleAuth,
+  signOut,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.post("/g-auth", googleAuth);
+router.get("/signout", signOut);
 
 export default router;
