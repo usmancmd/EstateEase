4f84bc2ed3457183959a002e52108560ee9885c1
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-10 07:17:20 +0100

    Add getUserListing and deleteUser controller

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,9 +1,9 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 
 export const updateUser = async (req, res, next) => {
-  console.log("log from updateUser", req.user.id);
   if (req.user.id !== req.params.id)
     return next(errorHandler(403, "Unauthorized"));
 
@@ -31,5 +31,30 @@ export const updateUser = async (req, res, next) => {
       .json({ response_msg: "user updated successfully!!", response }); // {"response": "user updated successfully!!"}
   } catch (error) {
     next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "Unauthorized"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("user deleted successfully!!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "Unauthorized"));
   }
 };
