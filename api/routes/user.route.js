98a0a4081d201421f2c37970a914cb3dead4ab3c
Usman Adepoju <adepojuusman465@gmail.com>
2023-12-11 10:26:19 +0100

    update get user listing api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   deleteUser,
   updateUser,
-  getUserListing,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -10,6 +10,6 @@ const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/listings/:id", verifyToken, getUserListing);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
